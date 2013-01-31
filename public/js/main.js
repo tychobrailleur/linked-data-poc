686d7815329ca1f2543ffb4b777cd478a1dcbfee
Sebastien Le Callonnec <sebastien@weblogism.com>
2013-01-31 19:46:25 +0000

    Finish migration to knockout.js.

--- public/js/main.js
+++ public/js/main.js
@@ -2,54 +2,61 @@ $.ajaxSetup ({
   cache: false
 });
 
-var CompanyReportModel = {
-  name: ko.observable(),
-  duns: ko.observable(),
-  address: ko.observable(),
-  dbpedia: ko.observable(),
-  description: ko.observable(),
-  show: ko.observable(),
-  getReport: function() {
-    CompanyReportModel.show(false);
-    var q = $("#q").val();
+var CompanyReportModel = function() {
+  self = this;
+
+  // Search input text
+  this.input = ko.observable();
+  this.name = ko.observable();
+  this.duns = ko.observable();
+  this.address = ko.observable();
+  this.dbpedia = ko.observable();
+  this.description = ko.observable();
+
+  // Variable to toggle results div display.
+  this.show = ko.observable();
+
+  this.getReport = function() {
+    this.show(false);
     $.getJSON(
-      "/match/" + q,
+      "/match/" + this.input(),
       function(data) {
-        renderReport(data);
+        self.renderReport(data);
       }
-    );    
-  }
-};
-ko.applyBindings(CompanyReportModel);
+    );
+  };
 
-function renderReport(data) {
-  if (data) {
-    CompanyReportModel.name(data["http://schema.org/Corporationname"]);
-    CompanyReportModel.duns(data["http://schema.org/Corporationduns"]);
-    CompanyReportModel.address(data["http://schema.org/PostalAddressstreetAddress"]);
-
-    // hardcoded, as the sparql query returns dbpedia for linked nodes too.
-    CompanyReportModel.dbpedia(data["http://www.w3.org/2002/07/owl#sameAs"][1]);
-    getDbPedia(data["http://www.w3.org/2002/07/owl#sameAs"][1]);
-
-    CompanyReportModel.show(true);
-  }
-}
-
-function getDbPedia(entityId) {
-  console.log("DbPedia! " + entityId);
-  $.getJSON(
-    "/dbpedia/" + encodeURIComponent(entityId),
-    function(data) {
-      renderDbPedia(data, entityId);
+  this.renderReport = function(data) {
+    if (data) {
+      this.name(data["http://schema.org/Corporationname"]);
+      this.duns(data["http://schema.org/Corporationduns"]);
+      this.address(data["http://schema.org/PostalAddressstreetAddress"]);
+      
+      // hardcoded, as the sparql query returns dbpedia for linked nodes too.
+      this.dbpedia(data["http://www.w3.org/2002/07/owl#sameAs"][1]);
+      this.getDbPedia(data["http://www.w3.org/2002/07/owl#sameAs"][1]);
+      
+      this.show(true);
     }
-  );
-}
+  };
 
-function renderDbPedia(data, uri) {
-  if (data.results && data.results.bindings.length > 0) {
-    CompanyReportModel.description(data.results.bindings[0].abstract.value);
-    //var logo = $("#report-logo");
-    //logo.attr("src", data.results.bindings[0].logo.value);
-  }
-}
+  this.getDbPedia = function(entityId) {
+    console.log("DbPedia! " + entityId);
+    $.getJSON(
+      "/dbpedia/" + encodeURIComponent(entityId),
+      function(data) {
+        self.renderDbPedia(data, entityId);
+      }
+    );
+  };
+
+  this.renderDbPedia = function(data, uri) {
+    if (data.results && data.results.bindings.length > 0) {
+      this.description(data.results.bindings[0].abstract.value);
+      //var logo = $("#report-logo");
+      //logo.attr("src", data.results.bindings[0].logo.value);
+    }
+  };
+};
+
+ko.applyBindings(new CompanyReportModel());
